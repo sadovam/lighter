200a619516ebdb78dee493713601fb3d00b09b4a
Sadov Andrii <sadovam@gmail.com>
2021-11-05 21:50:33 +0200

    Added string representation for models of app links

--- lighter/links/models.py
+++ lighter/links/models.py
@@ -7,12 +7,19 @@ class LinkPage(models.Model):
     description = models.CharField(max_length=255, blank=True)
     is_public = models.BooleanField(default=True)
 
+    def __str__(self):
+        return self.name
+
 class LinkGroup(models.Model):
     author = models.ForeignKey('auth.User', on_delete=models.CASCADE)
     name = models.CharField(max_length=30)
     note = models.TextField(blank=True)
     is_public = models.BooleanField(default=True)
     page = models.ForeignKey(LinkPage, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.name
+
 
 class Link(models.Model):
     author = models.ForeignKey('auth.User', on_delete=models.CASCADE)
@@ -22,3 +29,6 @@ class Link(models.Model):
     add_date = models.DateTimeField('Add Date', default=timezone.now)
     is_public = models.BooleanField(default=True)
     group = models.ForeignKey(LinkGroup, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.name
