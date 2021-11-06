f4bf6cde371267f1ecddf9c9fc1c4904c0b83fad
Sadov Andrii <sadovam@gmail.com>
2021-11-06 20:15:16 +0200

    Added naive url-view-template for start page of links app

--- lighter/links/views.py
+++ lighter/links/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
 
+from .models import LinkPage
+
 # Create your views here.
+def links_index(request):
+    return render(request, 'links/index.html', {'link_pages_list': LinkPage.objects.all()})
